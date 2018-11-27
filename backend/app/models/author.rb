e156bcc48c43cdc2145f6bb38156731eb5cae50b
Hovhannes <h.karapetyan555@gmail.com>
2018-11-27 17:56:21 +0400

    add additional validations, update config

--- backend/app/models/author.rb
+++ backend/app/models/author.rb
@@ -1,6 +1,7 @@
 class Author < ApplicationRecord
   has_many :books
   validates :fullName, :email, presence: true
+  validates :fullName, length: { in: 1..25 }
   validates :email, format: /@/
   validates :email, uniqueness: { message: "Email has been taken,please try with another email"}
 end
--- backend/app/models/book.rb
+++ backend/app/models/book.rb
@@ -2,4 +2,5 @@ class Book < ApplicationRecord
   belongs_to :author
   validates :title, :author_id, presence: true #description and rating are optional
   validates :rating, :inclusion => { :in => 0..10 }
+  validates :title, length: {in: 1..60}
 end
